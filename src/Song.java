1f9e2df7a88336dfb1a98b4c0c8f9923c7c8e3d7
AmbyH <kinghah2026@student.cis.edu.hk>
2024-02-27 10:09:25 +0800

    i hate taylor swift

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+
+    private ArrayList<String> old;
+
+    public Song(String a, String b) {
+        title = a;
+        artist = b;
+        old = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> listeners) {
+        int count = 0;
+        for (String listener : listeners) {
+            if (!old.contains(listener.toLowerCase())) {
+                old.add(listener.toLowerCase());
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 }
